5461438c7f6a586e5ae36876a1bbb0ed41aff555
ikhmalhariryyy <ikhmalhariry@gmail.com>
2026-08-30 05:01:02 +0800

    Update 02_data_cleaning.R

--- R/02_data_cleaning.R
+++ R/02_data_cleaning.R
@@ -1,3 +1,16 @@
 read.csv(customer_shopping_behavior)
 
 colSums(is.na(customer_shopping_behavior))
+
+           Customer ID                    Age                 Gender 
+                     0                      0                      0 
+        Item Purchased               Category  Purchase Amount (USD) 
+                     0                      0                      0 
+              Location                   Size                  Color 
+                     0                      0                      0 
+                Season          Review Rating    Subscription Status 
+                     0                     37                      0 
+         Shipping Type       Discount Applied        Promo Code Used 
+                     0                      0                      0 
+    Previous Purchases         Payment Method Frequency of Purchases 
+                     0                      0                      0
